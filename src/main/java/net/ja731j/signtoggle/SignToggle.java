92e3c8bec86549392dab2aea16ebd8e20f4d8d32
syamn <admin@sakura-server.net>
2013-01-23 12:29:24 +0900

    Update

--- src/main/java/net/ja731j/signtoggle/SignToggle.java
+++ src/main/java/net/ja731j/signtoggle/SignToggle.java
@@ -1,11 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.ja731j.signtoggle;
 
 import java.util.HashSet;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -22,8 +19,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author ja731j
  */
 public class SignToggle extends JavaPlugin implements Listener {
-
-    static HashSet<String> inuse;
+    private static HashSet<String> inuse;
 
     @Override
     public void onEnable() {
@@ -34,16 +30,22 @@ public class SignToggle extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
+        inuse.clear();
         getLogger().info("The SignToggle plugin has been unloaded");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player) || !cmd.getName().equalsIgnoreCase("signtoggle")) {
+        if (!cmd.getName().equalsIgnoreCase("signtoggle")) {
             return false;
         }
-        Player p = (Player) sender;
-        if (!p.hasPermission("signtoggle")) {
+        if (!(sender instanceof Player)){
+            sender.sendMessage("This command cannot run from console!");
+            return true;
+        }
+        
+        final Player p = (Player) sender;
+        if (!p.hasPermission("signtoggle.use")) {
             p.sendMessage("You don't have the permissions!");
             return true;
         }
@@ -59,9 +61,12 @@ public class SignToggle extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onHit(PlayerInteractEvent e) {
-
-        if (e.getPlayer() == null || !e.getPlayer().hasPermission("signtoggle")) {
+    public void onHit(final PlayerInteractEvent e) {
+        if (e.isCancelled()){
+            return;
+        }
+        
+        if (e.getPlayer() == null || !e.getPlayer().hasPermission("signtoggle.use")) {
             return;
         }
 
@@ -70,15 +75,17 @@ public class SignToggle extends JavaPlugin implements Listener {
             return;
         }
 
-        Block b = e.getClickedBlock();
+        final Block b = e.getClickedBlock();
 
         //If target block is not null and is some kind of sign, proceed
-        if (b != null
-                && (b.getType() == Material.WALL_SIGN || b.getType() == Material.SIGN_POST)) {
-
+        if (b != null  && (b.getType() == Material.WALL_SIGN || b.getType() == Material.SIGN_POST)) {
             //Get sign data
             org.bukkit.block.Sign s = (org.bukkit.block.Sign) b.getState();
             String[] lines = s.getLines();
+            if (lines[0].trim().equalsIgnoreCase("[private]")){
+                e.getPlayer().sendMessage("This is a private sign");
+                return;
+            }
             BlockFace face = ((org.bukkit.material.Sign) (s.getData())).getFacing();
 
             //Switch between sign posts and wall signs
@@ -94,12 +101,16 @@ public class SignToggle extends JavaPlugin implements Listener {
             //Set sign data
             ((org.bukkit.material.Sign) (s.getData())).setFacingDirection(face);
             for (int i = 0; i < 4; i++) {
-
                 s.setLine(i, lines[i]);
             }
             s.update();
 
+            // simply take a log
+            final Location l = b.getLocation();
+            if (!l.getWorld().getName().equals("entrance")){
+                System.out.println("[SignToggle] " + e.getPlayer().getName() + " toggled sign at ("
+                                   + l.getWorld().getName() + ": "+l.getBlockX()+", "+l.getBlockY()+", "+l.getBlockZ()+")");
+            }
         }
-
     }
 }
